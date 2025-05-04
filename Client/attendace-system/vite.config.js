f3e0c802fd7b9f0a3d0d537557e846d48aa7082d
imCarl01 <imcarl0.uc@gmail.com>
2025-05-04 09:22:31 -0700

    added a new form of header

--- Client/attendace-system/vite.config.js
+++ Client/attendace-system/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  build:{
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [react(),tailwindcss()],
 })
